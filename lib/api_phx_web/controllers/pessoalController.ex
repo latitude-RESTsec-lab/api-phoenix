da8ca5a65d3de06498c7bbc6aa7afa98f089a2f4
Rafael Auyer <rafael.passos@redes.unb.br>
2018-01-15 14:11:36 -0200

    Get by "matricula"  controller

--- lib/api_phx_web/controllers/pessoalController.ex
+++ lib/api_phx_web/controllers/pessoalController.ex
@@ -29,7 +29,25 @@ defmodule ApiPhxWeb.PessoalController do
       
       
     end
-  
-  
 
+    def getPessoalMat(conn, parameters) do
+        Map.get(parameters, "matricula")  |> IO.inspect
+        case  Map.get(parameters, "matricula") |> String.match?( ~r{\b[0-9]+\b} ) do
+            true ->
+                query = "select s.id_servidor, s.siape, s.id_pessoa, s.matricula_interna, s.nome_identificacao,
+                    p.nome, p.data_nascimento, p.sexo from rh.servidor s
+                    inner join comum.pessoa p on (s.id_pessoa = p.id_pessoa) where s.matricula_interna = #{Map.get(parameters, "matricula")}"         
+                case Ecto.Adapters.SQL.query(ApiPhx.Repo, query) do
+                    {:ok, res} -> 
+                        pessoal = Enum.map( res.rows , fn(row) ->
+                        Enum.zip( res.columns, row |> List.update_at( 6 ,fn(tup) ->
+                            :io_lib.format("~4..0B-~2..0B-~2..0B", Tuple.to_list(tup)) |> List.flatten |> to_string
+                        end)) |> Map.new
+                    end) 
+                    json conn_with_status(conn, pessoal), pessoal
+                    {status, _} -> IO.inspect(status)
+                end
+            false -> json conn_with_status(conn, nil), nil
+        end
+    end
   end
