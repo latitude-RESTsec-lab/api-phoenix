62490fdfe2f2b2f96af9b74e735dcccbbbaba686
Rafael Auyer <rafael.passos@redes.unb.br>
2018-04-06 16:30:17 -0300

    Modified byID querry

--- lib/api_phx_web/controllers/pessoalController.ex
+++ lib/api_phx_web/controllers/pessoalController.ex
@@ -22,11 +22,11 @@ defmodule ApiPhxWeb.ServidorController do
                     Enum.zip( res.columns, row |> List.update_at( 6 ,fn(tup) ->
                         :io_lib.format("~4..0B-~2..0B-~2..0B", Tuple.to_list(tup)) |> List.flatten |> to_string
                     end)) |> Map.new #Converting to type Map (basicly a JSON)
-                end) 
+                end)
                 json conn_with_status(conn, 200), servidor
             {:error , reason} -> # in case the query fails
                 json conn_with_status(conn, 500), %{"Reason" => reason}
-        end      
+        end
     end
     def getServidorMat(conn, parameters) do
         #Regex to check if parameter is number
@@ -34,16 +34,16 @@ defmodule ApiPhxWeb.ServidorController do
             true ->
                 query = "select s.id_servidor, s.siape, s.id_pessoa, s.matricula_interna, s.nome_identificacao,
                     p.nome, p.data_nascimento, p.sexo from rh.servidor s
-                    inner join comum.pessoa p on (s.id_pessoa = p.id_pessoa) where s.matricula_interna = #{Map.get(parameters, "matricula")}"         
+                    inner join comum.pessoa p on (s.id_pessoa = p.id_pessoa) where s.siape = #{Map.get(parameters, "matricula")}"
                 case Ecto.Adapters.SQL.query(ApiPhx.Repo, query) do
-                    {:ok, res} -> 
+                    {:ok, res} ->
                         servidor = Enum.map( res.rows , fn(row) -> #iteration over array to format dates acordingly to requisites
                         Enum.zip( res.columns, row |> List.update_at( 6 ,fn(tup) ->
                             :io_lib.format("~4..0B-~2..0B-~2..0B", Tuple.to_list(tup)) |> List.flatten |> to_string
                         end)) |> Map.new
-                    end) 
+                    end)
                         json conn_with_status(conn, 200), servidor
-                    {:error, error} -> 
+                    {:error, error} ->
                         json conn_with_status(conn, 500), %{"Reason" => error}
                 end
             false -> json conn_with_status(conn, 404), nil
@@ -62,9 +62,9 @@ defmodule ApiPhxWeb.ServidorController do
             {_,dateNow} = Date.new(elem(dateNow,0), elem(dateNow,1), elem(dateNow,2))
             if Date.compare(dateJson,dateNow) == :gt do
                 {error, reasons} = {True, reasons ++ [%{"Reason" => "[data_nascimento] missing or failed to match API requirements. It should not be in future"}]}
-            end  
+            end
         end
-        if not Regex.match?(~r/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/, Map.get(parameters, "nome", "default")) do            
+        if not Regex.match?(~r/^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$/, Map.get(parameters, "nome", "default")) do
             {error, reasons} = {True, reasons ++ [%{"Reason" => "[name] missing or failed to match API requirements. It should look like this: Firstname Middlename(optional) Lastname"}]}
         end
         if String.length(Map.get(parameters, "nome", "default")) > 100 do
@@ -95,15 +95,30 @@ defmodule ApiPhxWeb.ServidorController do
             IO.inspect query = "INSERT INTO rh.servidor_tmp(
                 nome, nome_identificacao, siape, id_pessoa, matricula_interna, id_foto,
                 data_nascimento, sexo)
-                VALUES ('#{Map.get(parameters, "nome")}', '#{Map.get(parameters, "nome_identificacao")}', #{Map.get(parameters, "siape")}, #{Map.get(parameters, "id_pessoa")}, #{mat}, null, 
+                VALUES ('#{Map.get(parameters, "nome")}', '#{Map.get(parameters, "nome_identificacao")}', #{Map.get(parameters, "siape")}, #{Map.get(parameters, "id_pessoa")}, #{mat}, null,
                 '#{Map.get(parameters, "data_nascimento")}', '#{Map.get(parameters, "sexo")}');"
                 case Ecto.Adapters.SQL.query(ApiPhx.Repo, query) do
-                    {:ok, res} -> 
+                    {:ok, res} ->
                         conn = %{conn | resp_headers: [{"location", (ApiPhxWeb.Router.Helpers.url(conn) <> "/api/servidor/" <> Integer.to_string(mat))}]}
                         text conn_with_status(conn, 201), nil
-                    {:error, error} -> 
+                    {:error, error} ->
                         json conn_with_status(conn, 500), %{"Reason" => error}
                 end
         end
+    end
+    def calculate(conn, parameters) do
+        reasons = []
+        error = False
+        #REGEX checking
+        IO.puts(Map.get(parameters))
+        #END OF REGEX
+
+        # if
+        #     {:ok, res} ->
+        #         conn = %{conn | resp_headers: [{"location", (ApiPhxWeb.Router.Helpers.url(conn) <> "/api/servidor/" <> Integer.to_string(mat))}]}
+        #         text conn_with_status(conn, 201), nil
+        #     {:error, error} ->
+        #         json conn_with_status(conn, 500), %{"Reason" => error}
+        # end
     end
   end
